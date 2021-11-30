170546274a35cc5d4cbc26953c29cdafec011ab6
root <root@RAIDEN.cybercloud.local>
2021-11-30 17:48:26 -0500

    If node is leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+int binary_tree_is_leaf(const binary_tree_t *node);
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+
+	if (node == NULL)
+		return (0);
+	if (node->left != NULL)
+		return (0);
+	if (node->right != NULL)
+		return (0);
+	return (1);
+}
