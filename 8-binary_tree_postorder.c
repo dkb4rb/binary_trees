93c7a7635fa77813aff8e97da5818709998d0d1c
root <root@RAIDEN.cybercloud.local>
2021-11-30 18:48:19 -0500

    Test

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func != NULL)
+	{
+		binary_tree_postorder(tree->left, func);
+		func(tree-n);
+		binary_tree_postorder(tree->right, func);
+	}
+}
--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,8 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
-#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -25,5 +26,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 int binary_tree_is_leaf(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* _BINARY_TREES_H_ */
