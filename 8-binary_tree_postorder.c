251fe1a0380c1d02b5922043af3d7b31a95b31db
root <root@RAIDEN.cybercloud.local>
2021-11-30 20:25:19 -0500

    Update new depth nodes size

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -2,9 +2,15 @@
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
+/**
+* binary_tree_postorder - goes through a binary tree using post-order traversal
+* @tree: is ponter to binary_trees
+* @func: this is all pointer to functions the nodes
+*/
+
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if(tree && func)
+	if (tree && func)
 	{
 		binary_tree_postorder(tree->left, func);
 		func(tree->n);
