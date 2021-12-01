8b1302f5a993548ee828a464c50a70803723c813
root <root@RAIDEN.cybercloud.local>
2021-11-30 19:49:05 -0500

    Node postorder

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,7 +12,6 @@
 */
 
 
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -4,10 +4,11 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree && func != NULL)
+	if(tree && func)
 	{
 		binary_tree_postorder(tree->left, func);
-		func(tree-n);
+		func(tree->n);
 		binary_tree_postorder(tree->right, func);
 	}
+
 }
