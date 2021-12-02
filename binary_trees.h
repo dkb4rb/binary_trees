3a569efb63ccb4b36abb98d047f7a6a4b20bf315
LuisaPinillos <luisapinillos20@gmail.com>
2021-12-01 21:29:42 -0500

    update

--- binary_trees.h
+++ binary_trees.h
@@ -27,9 +27,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
-size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 #endif /* _BINARY_TREES_H_ */
