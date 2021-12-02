00f1f1b2ef53afbc6eecc55f341ab44bb7adeecb
root <root@RAIDEN.cybercloud.local>
2021-12-01 23:00:57 -0500

    is node perfect

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,46 @@
+#include "binary_trees.h"
+
+int binary_tree_is_perfect(const binary_tree_t *tree);
+int tree_height(const binary_tree_t *tree);
+
+/**
+* tree_height - measures the hight of the tree
+* @tree:pointer to the root node of the tree
+* Return: 0 if tree is NULL else the hight
+*/
+
+int tree_height(const binary_tree_t *tree)
+{
+	int h_l, h_r;
+
+	if (tree == NULL)
+		return (0);
+
+	h_l = tree_height(tree->left);
+	h_r = tree_height(tree->right);
+
+	if (h_l >= h_r)
+		return (h_l + 1);
+
+	return (h_r + 1);
+}
+/**
+* binary_tree_is_perfect - measures the balance factor of a binary tree
+* @tree: a pointer to the root node of the tree to measure the balance factor
+* Return: 0 or balance factor
+*/
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (tree == NULL)
+		return (0);
+
+	left = tree_height(tree->left);
+	right = tree_height(tree->right);
+
+	if (left != right)
+		return (0);
+	return (1);
+}
