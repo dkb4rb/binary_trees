aa7c7d508c77b5baf43f431fbca557edf132bd27
LuisaPinillos <luisapinillos20@gmail.com>
2021-12-01 19:47:38 -0500

    Fix comments

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -4,7 +4,7 @@
  * binary_tree_size - function that measures the
  * size of a binary tree.
  * @tree: pointer to the root node of the tree to traverse.
- * Return: nothing.
+ * Return: Always 0 (Success)
  */
 
 size_t binary_tree_size(const binary_tree_t *tree)
